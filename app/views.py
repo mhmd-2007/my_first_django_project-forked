3baeecffb6ebeff8b1bfde91b60cded6ef3c893f
mhmdin <mhmdin2007@gmail.com>
2026-05-24 17:36:36 +0330

    working on view and add a function to it. create directory myproject/app/template/. and endit flile myproject/urls.py.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Survey
 
 # Create your views here.
+
+def survey_list(request):
+    surveys = Survey.objects.filter(is_active=True)
+    return render(request, "app/survey_list.html", {"surveys": surveys})
