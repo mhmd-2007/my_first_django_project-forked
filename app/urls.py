57b05a8d96b5a260e022e96cd4bdd79bd31f8aef
mhmdin <mhmdin2007@gmail.com>
2026-05-25 14:29:07 +0330

    adding functions vote,... and editi web html and fix ursl.py

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.survey_list, name='survey_list'),    
+    path('', views.survey_list, name='survey_list'),
+    path('survey/<int:survay_id>/', views.survey_detail, name='survey_detail'),   
+    path('survey/<int:survey_id>/vote', views.vote, name='vote'), 
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
-from .models import Survey
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Survey, Choice
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 def survey_list(request):
     surveys = Survey.objects.filter(is_active=True)
     return render(request, "app/survey_list.html", {"surveys": surveys})
+
+def survey_detail(request, survey_id):
+    survey = get_object_or_404(Survey, id=survey_id, is_active=True)
+    choices = survey.choice_set.all()
+    return render(request, "app/survey_detail.html", {
+        'survey': survey,
+        'choices': choices,
+    })
+
+def vote(request, survey_id):
+    survey = get_object_or_404(Survey, pk=survey_id, is_active=True)
+
+    try:
+        selected_choice = survey.choice_set.get(pk=request.POST['choice'])
+    except:
+        choices = survey.choice_set.all()
+        return render(request, 'app/survey_detail.html', {
+            'survey' : survey,
+            'choices' : choices,
+            'error_message' : "لطفا یکی از گزینه های زیر را انتخاب کنی.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('results', args=(survey.id,)))
+    
